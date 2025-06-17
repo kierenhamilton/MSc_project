9ffcc6c404c94e6a1818bdd685fe6a030e14ff52
kierenhamilton <kierenhamilton1@outlook.com>
2025-06-17 22:23:14 +0100

    ram fixed

--- rtl/ram.sv
+++ rtl/ram.sv
@@ -16,16 +16,21 @@ module ram (
         3'b000: ramMemory[address] <= wData[7:0];
         3'b001: {ramMemory[address+1], ramMemory[address]} <= wData[15:0];
         3'b010:
-        {ramMemory[address+3], ramMemory[address+2], ramMemory[address+1], ramMemory[address]} <= wData;
+        {ramMemory[address+3], ramMemory[address+2], ramMemory[address+1], ramMemory[address]}
+        <= wData;
         default: begin
         end
       endcase
   end
 
   always_comb begin
+    rData = 0;
     case (ctrl)
-      3'b000: rData = $signed(ramMemory[address]);  // load byte
-      3'b001: rData = $signed({ramMemory[address+1], ramMemory[address]});  // load half-word
+      3'b000: rData = {{24{ramMemory[address][7]}}, ramMemory[address]};  // load byte
+      3'b001:
+      rData = {
+        {16{ramMemory[address+1][7]}}, ramMemory[address+1], ramMemory[address]
+      };  // load half-word
 
       3'b010:  // load word
       rData = {
